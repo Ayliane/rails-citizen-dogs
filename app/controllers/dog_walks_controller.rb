eb4a3bf7c86891927f2f4fbd62484b38590671b1
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-07-20 15:59:30 +0200

    creating home and routes

--- app/controllers/dog_walks_controller.rb
+++ app/controllers/dog_walks_controller.rb
@@ -1,2 +1,23 @@
 class DogWalksController < ApplicationController
+  def index
+    @dog_walks = dog_walk.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
