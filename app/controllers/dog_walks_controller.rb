afcc8318e2f32792353f082e5f01402b191dc810
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-07-21 16:38:28 +0200

    dry

--- app/controllers/dog_walks_controller.rb
+++ app/controllers/dog_walks_controller.rb
@@ -1,13 +1,21 @@
 class DogWalksController < ApplicationController
+  before_action :set_dog_walk, only: [:show, :edit, :update, :destroy]
+
   def index
     @dog_walks = DogWalk.all
   end
 
-  def create
+  def new
     @dog_walk = DogWalk.new
   end
 
-  def new
+  def create
+    @dog_walk = DogWalk.new(params[dog_walk_params])
+    if @dog_walk.save
+      redirect_to dog_walk_path(@dog_walk)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,8 +25,27 @@ class DogWalksController < ApplicationController
   end
 
   def update
+    if @dog_walk.update(params[dog_walk_params])
+      redirect_to dog_walk_path(@dog_walk)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @dog_walk.destroy
+
+    redirect_to dog_walks_path
   end
+
+  private
+
+  def dog_walk_params
+    params.require(:dog_walk).permit(:title, :description, :date, :hour, :meeting_point, :available_spots)
+  end
+
+  def set_dog_walk
+    @dog_walk = DogWalk.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dog_walks
 
-  root to: "pages#home"
+  root to: "dog_walks#index"
+  # root to: "pages#home"
 end
